0eb21a54687253630dac0b1d8e39348530ff441f
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2023-01-07 13:48:28 +0200

    update main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,16 @@
 import React from "react"
+import AirportCard from "../components/AirportCard"
+import AirportFilter from "../components/AirportFilter"
+import AirportSearch from "../components/AirportSearch"
 
 export default function MainPage() {
-  return <div>main</div>
+  return (
+    <div className="container mx-auto max-w-[760px] pt-5">
+      <AirportSearch />
+
+      <AirportFilter />
+
+      <AirportCard />
+    </div>
+  )
 }
