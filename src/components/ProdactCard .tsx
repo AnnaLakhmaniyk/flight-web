5802e23a86fefd004428b396d4d3255edcd20335
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2023-01-10 21:39:41 +0200

    update product card

--- src/components/ProdactCard .tsx
+++ src/components/ProdactCard .tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 import { IProduct } from "../models"
 
@@ -8,8 +9,16 @@ interface ProdactCardProp {
 }
 
 export default function ProdactCard({ prod }: ProdactCardProp) {
+  const navigate = useNavigate()
+  const clicHendler = () => {
+    navigate(`/product/${prod.id}`)
+  }
+
   return (
-    <li className="mr-4 mb-4 rounded-sm border-2 border-stone-300 p-5">
+    <li
+      className="mr-4 mb-4 rounded-sm border-2 border-stone-300 p-5"
+      onClick={clicHendler}
+    >
       <img src={prod.image} alt="product" className="h-40 w-40" />
       <p>{prod.category}</p>
       <p>{prod.price}</p>
