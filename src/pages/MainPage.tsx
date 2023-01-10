ca5d5e63d5709cc9054f8829e5da5853aeaf3cfe
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2023-01-10 12:07:33 +0200

    main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,26 @@
 import React from "react"
-import AirportCard from "../components/AirportCard"
-import AirportFilter from "../components/AirportFilter"
-import AirportSearch from "../components/AirportSearch"
+import ProdactCard from "../components/ProdactCard "
+import ProductFilter from "../components/ProductFilter"
+
+import { useFetchProductQuery } from "../store/products/productsApi"
 
 export default function MainPage() {
+  const { isLoading, isError, data } = useFetchProductQuery(" ")
+  console.log(data)
+
   return (
     <div className="container mx-auto max-w-[760px] pt-5">
-      <AirportSearch />
+      {isError && (
+        <p className="text-center text-red-600">Something went wrong...</p>
+      )}
+      <ProductFilter />
 
-      <AirportFilter />
-
-      <AirportCard />
+      <ul className="flex flex-row flex-wrap">
+        {isLoading && <p className="text-center">Loading...</p>}
+        {data?.map((prod) => (
+          <ProdactCard prod={prod} key={prod.id} />
+        ))}
+      </ul>
     </div>
   )
 }
